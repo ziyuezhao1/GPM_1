5959f034eaff2cff53bcab98c01f7a15c40edd9e
ziyuezhao1 <112548074+ziyuezhao1@users.noreply.github.com>
2024-07-01 19:11:18 +0800

    Add files via upload

---
+++ experiment2_plot.py
@@ -0,0 +1,58 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.ticker import LogLocator as ll
+
+
+def calcMeanDF(data, index):
+    return data.pivot_table(index=index, aggfunc='mean').reset_index()
+
+
+
+if __name__=="__main__":
+    # Load data
+    data = pd.read_csv("experiment2-2024-07-01-181011.csv")
+
+    # Pivot data
+    pdata = calcMeanDF(data, ['p', 'sigma_t'])
+
+    # Plot everything
+    fig, axs = plt.subplots(2, 2, sharex=True, sharey='row', constrained_layout=True, figsize=(6.4, 3.48))
+
+    for pi, p in enumerate(pdata["p"].unique()):
+        subdata = pdata.loc[pdata["p"] == p]
+
+        axs[0, pi].set_title(" $ p = {:.2f} $ ".format(p))
+
+        for erri, err in enumerate(["rerr", "terr"]):
+            ax = axs[erri, pi]
+
+            if pi == 0:
+                if err == "rerr":
+                    ax.set_ylabel("Rotation Error")
+                else:
+                    ax.set_ylabel("Translation Error")
+
+            if erri == 1:
+                ax.set_xlabel("$ \\sigma_{t} $ ")
+
+            if erri == 0:
+                ax.set_ylim(1e-10, 3e0)
+
+            for prefix in ["dq", "mat","gpm_mat"]:
+                ax.semilogy(subdata["sigma_t"], subdata["_".join([prefix, "mean", err])], '-o', markersize=3, \
+                        label = prefix.upper() + ",mean", \
+                            lw = 0.5)
+                ax.fill_between(subdata["sigma_t"], \
+                                subdata["_".join([prefix, "min", err])], \
+                                subdata["_".join([prefix, "max", err])], \
+                                alpha = 0.3, \
+                                label = prefix.upper() + ",[min,max]")
+
+                ax.yaxis.set_major_locator(ll(numticks=50))
+
+            ax.grid(which="both", alpha = 0.3)
+    axs[-1, -1].legend(fontsize = 6, loc="upper left")
+
+    plt.savefig("experiment2_fig.pdf")
+    plt.show(block=False)
+
---
+++ experiment4_plot.py
@@ -0,0 +1,46 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+def calcMeanDF(data, index):
+    return data.pivot_table(index=index, aggfunc='mean').reset_index()
+
+
+
+if __name__=="__main__":
+    # Load data
+    data = pd.read_csv("experiment4-2024-07-01-181221.csv")
+
+    # Pivot data
+    pdata = calcMeanDF(data, ['q'])
+
+    # Plot everything
+    fig, axs = plt.subplots(1, 2, sharex=True, constrained_layout=True)
+
+    for erri, err in enumerate(["rerr", "terr"]):
+        ax = axs.flatten()[erri]
+
+        if err == "rerr":
+            ax.set_ylabel("Rotation Error")
+        else:
+            ax.set_ylabel("Translation Error")
+
+        for prefix in ["dq", "mat","gpm_mat"]:
+            ax.semilogy(pdata["q"][:-1], pdata["_".join([prefix, "mean", err])][:-1], '-o', markersize=3, \
+                    label = prefix.upper() + ",mean", \
+                        lw = 0.5)
+            ax.fill_between(pdata["q"][:-1], \
+                            pdata["_".join([prefix, "min", err])][:-1], \
+                            pdata["_".join([prefix, "max", err])][:-1], \
+                            alpha = 0.3, \
+                            label = prefix.upper() + ",[min,max]")
+
+        ax.grid(which="both", alpha = 0.3)
+    axs[-1].legend(fontsize = 8, loc = "lower left")
+
+    fig.supxlabel("Percent of non-corrupted entries ($ q $)")
+
+    fig.set_size_inches(7.75, 3.4)
+    
+    plt.savefig("experiment4_fig.pdf")
+    plt.show(block=False)
